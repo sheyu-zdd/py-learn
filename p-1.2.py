1e3483aa9a86458b46966a2a04afde4eaf2d3091
sheyu <19541986820@163.com>
2026-08-14 15:23:22 +0800

    feat:p-1.2

---
+++ p-1.2.py
@@ -0,0 +1,11 @@
+# 字面量
+
+print(3) # 整数（int）
+print(3.14) # 浮点数/小数（float）
+print(True) # 布尔（boolean,首字母大写）
+print("Hello python") #字符串（String）
+print(None) # 空值(NoneType)
+
+# boolean 本质也是整数，True表示1，False表示0
+print(True+1) # 2
+print(False - 1) # -1
